a4e8d729959ab111c41a0f10131361b8b2a5c926
David Leach <davidleach724@gmail.com>
2021-05-14 12:37:43 -0600

    Pass meal tests

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,71 @@
-function nameMenuItem() {
+function nameMenuItem(name) {
+  var deliciousName = `Delicious ${name}`;
 
+  return deliciousName;
+}
+
+
+function createMenuItem(name, price, type) {
+  var menuItem = {
+    name: name,
+    price: price,
+    type: type
+  }
+
+  return menuItem;
+}
+
+
+function addIngredients(toppings, ingredientsArray) {
+
+//Runs through the array and compares toppings with each value in the ingredientsArray
+for (i=0; i<ingredientsArray.length; i) {
+//if toppings is equal to the value of the current index, it will return the array as is
+  if (toppings === ingredientsArray[i]) {
+    return ingredientsArray;
+//if it is not equal, it will increase the increment expression and keep going through the loop
+  } else {
+    i++;
+  }
+}
+//if the loop finishes and has not found duplicate, thus returned, the topping will be added to the array
+// and then returned
+  ingredientsArray.push(toppings);
+
+  return ingredientsArray;
+}
+
+
+function formatPrice(price) {
+  var formattedPrice = `$${price}`;
+
+  return formattedPrice;
+}
+
+
+function decreasePrice(price) {
+  var discountedPrice = price*.9;
+
+  return discountedPrice;
+}
+
+
+function createRecipe(title, ingredients, menuItemType) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType
+  }
+
+  return recipe;
 }
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
